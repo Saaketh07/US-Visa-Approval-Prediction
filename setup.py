d5f5c2ea4208e48cbec73622a656df0f8d0a49d1
Saaketh <navurusaisaaketh@gmail.com>
2024-02-29 11:18:14 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="saaketh",
+    author_email="navurusaisaaketh@gmail.com",
+    packages=find_packages()
+)
